fc7d73d47ab8a1e2d4cc2228377c55ffc1f52256
Samuel Munyi <samunyi90@gmail.com>
2023-03-18 22:15:21 +0300

    Fix cors error

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://morning-dew-backend-production.up.railway.app/';
 
 ReactDOM.render(
   <React.StrictMode>
